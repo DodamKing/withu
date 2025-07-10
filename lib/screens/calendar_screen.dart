1709c8313e3409c3bfa6f2a830fbb5dd4b674f72
DodamKing <power6120@naver.com>
2025-07-10 15:20:53 +0900

    달력 헤더 폰트 조정, v2 배포

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -249,7 +249,7 @@ class CalendarScreenState extends State<CalendarScreen> { // public으로 변경
                           ),
                         ),
                         titleTextStyle: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.055, // 화면 너비의 5.5%
+                          fontSize: MediaQuery.of(context).size.width * 0.045, // 화면 너비의 5.5%
                           fontWeight: FontWeight.bold,
                           color: Color(0xFF1F2937),
                         ),
